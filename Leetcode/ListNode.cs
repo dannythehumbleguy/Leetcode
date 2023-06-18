5c85650c79fc8c523a4592090a027b8250be7540
Danil Zhiltsov <dannygam47@gmail.com>
2023-06-18 21:14:21 +0300

    19 v1

---
+++ Leetcode/ListNode.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+namespace Leetcode;
+
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+
+    public ListNode(int val = 0, ListNode next = null)
+    {
+        this.val = val;
+        this.next = next;
+    }
+
+    public override string ToString()
+    {
+        var sb = new StringBuilder();
+        var current = this;
+        while (current != null)
+        {
+            sb.Append(current.val + " ");
+            current = current.next;
+        }
+
+        return sb.ToString().Trim();
+    }
+}
---
+++ Leetcode/Medium/19.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+
+namespace Leetcode.Medium;
+
+public class Solution19_1 {
+    
+    [TestCase(5, "2 3 4 5")]
+    [TestCase(4, "1 3 4 5")]
+    [TestCase(3, "1 2 4 5")]
+    [TestCase(2, "1 2 3 5")]
+    [TestCase(1, "1 2 3 4")]
+    public void BaseTests(int n, string expected)
+    {
+        var head = new ListNode(1,
+            new ListNode(2,
+                new ListNode(3,
+                    new ListNode(4,
+                        new ListNode(5)))));
+        
+        Assert.AreEqual(expected, RemoveNthFromEnd(head, n).ToString());
+    }
+
+    [Test]
+    public void CornerCase() => Assert.AreEqual(null, RemoveNthFromEnd(new ListNode(555), 1));
+    
+    public ListNode RemoveNthFromEnd(ListNode head, int n)
+    {
+        var current = head;
+        var fake = new ListNode(-1, head);
+        var beforeTargetNode = fake;
+
+        while (current != null)
+        {
+            if (n > 0)
+                n--;
+            else
+                beforeTargetNode = beforeTargetNode.next;
+
+            current = current.next;
+        }
+
+        if (beforeTargetNode.next != null)
+            beforeTargetNode.next = beforeTargetNode.next?.next;
+        
+        return fake.next;
+    }
+}
