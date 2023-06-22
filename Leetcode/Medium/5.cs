b42e6a5c147241019896419e33523a07704105fc
Danil Zhiltsov <dannygam47@gmail.com>
2023-06-22 23:17:17 +0300

    5 v2

--- Leetcode/Medium/5.cs
+++ Leetcode/Medium/5.cs
@@ -1,7 +1,9 @@
-﻿using NUnit.Framework;
+﻿using System.Text;
+using NUnit.Framework;
 
 namespace Leetcode.Medium;
 
+// Solution with expand palindrome from center
 public class Solution5_1 
 {
     [TestCase("babad", "bab")]
@@ -58,5 +60,71 @@ public class Solution5_1
         }
 
         return (left + 1, right - 1);
+    }
+}
+
+// Solution with Manacher's algorithm
+public class Solution5_2
+{
+    [TestCase("babad", "bab")]
+    [TestCase("cbbd", "bb")]
+    [TestCase("ccc", "ccc")]
+    [TestCase("cccc", "cccc")]
+    public void BaseTests(string input, string expected)
+    {
+        Assert.AreEqual(expected, LongestPalindrome(input));
+    }
+    
+    public string LongestPalindrome(string input)
+    {
+        var a = new char[input.Length * 2 + 3];
+        
+        a[0] = '$';
+        a[1] = '#';
+        a[^1] = '@';
+        
+        var c = 0;
+        var radius = 0;
+        var palindromeMask = new int[a.Length];
+        
+        var t = 2;
+        foreach (var cq in input) {
+            a[t++] = cq;
+            a[t++] = '#';
+        }
+
+        for(var i = 1; i < palindromeMask.Length - 1; i ++)
+        {
+            if(i < radius)
+                palindromeMask[i] = Math.Min(radius - i, palindromeMask[2 * c - i]);
+
+            var left =  a[i + palindromeMask[i] + 1];
+            var right = a[i - palindromeMask[i] - 1];
+            while(left == right)
+            {
+                palindromeMask[i] += 1;
+                left =  a[i + palindromeMask[i] + 1];
+                right = a[i - palindromeMask[i] -1];
+            }
+            
+            if(i + palindromeMask[i]  > radius)
+            {
+                radius = i + palindromeMask[i];
+                c = i;
+            }
+        }
+        
+        var max = int.MinValue;
+        var index = 0;
+        for(var i = 1; i < palindromeMask.Length - 1; i++)
+        {
+            if (max >= palindromeMask[i]) 
+                continue;
+            
+            max = palindromeMask[i];
+            index = i;
+        }
+
+        return new string(a.Where((u, i) => i >= index - max && i <= index + max && u != '#').ToArray());
     }
 }
