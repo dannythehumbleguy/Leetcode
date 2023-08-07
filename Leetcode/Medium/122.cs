748ea37fd20cad26df7480707b8219fadafdfdaf
Danil Zhiltsov <dannygam47@gmail.com>
2023-08-07 12:47:17 +0300

    122

---
+++ Leetcode/Medium/122.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+
+namespace Leetcode.Medium;
+
+public class Solution122_1 
+{
+    [Test]
+    public void TestCase()
+    {
+        var input = new[] { 7, 1, 5, 3, 6, 4 };
+        var res = MaxProfit(input);
+        
+        Assert.AreEqual(7, res);
+    }
+    
+    public int MaxProfit(int[] prices)
+    {
+        var buy = int.MinValue;
+        var sell = 0;
+
+        foreach (var price in prices)
+        {
+            var nextBuy = Math.Max(buy, sell - price);
+            var nextSell = Math.Max(sell, buy + price);
+
+            buy = nextBuy;
+            sell = nextSell;
+
+            Console.WriteLine($"b: {buy}; s: {sell}");
+        }
+
+        return sell;
+    }
+}
