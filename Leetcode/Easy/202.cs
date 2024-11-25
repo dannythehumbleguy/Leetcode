e7cc19dae10405d579821ea919e51456204f5e8d
Danil Zhiltsov <dannygam47@gmail.com>
2024-11-25 23:56:12 +0300

    1907 and 202

---
+++ Leetcode/Easy/202.cs
@@ -0,0 +1,36 @@
+﻿using NUnit.Framework;
+
+namespace Leetcode.Easy;
+
+public class Solution202
+{
+    [TestCase(2, false)]
+    [TestCase(19, true)]
+    public void BaseTest(int input, bool expectedResult)
+    {
+        var result = IsHappy(input);
+        Assert.AreEqual(expectedResult, result);
+    }
+    
+    public bool IsHappy(int n)
+    {
+        var set = new HashSet<int>();
+        while (n != 1)
+        {
+            var copy = n;
+            n = 0;
+
+            while (copy != 0)
+            {
+                var lastNum = copy % 10;
+                copy /= 10;
+                n += lastNum * lastNum;
+            }
+
+            if (!set.Add(n))
+                return false;
+        }
+
+        return true;
+    }
+}
---
+++ Leetcode/Medium/1907.sql
@@ -0,0 +1,13 @@
+﻿-- noinspection SqlNoDataSourceInspectionForFile
+
+WITH rates AS (
+    SELECT case
+               when income < 20000 then 'Low Salary'
+               when income between 20000 and 50000 then 'Average Salary'
+               when income > 50000 then 'High Salary' END AS category,
+           1 AS is_real FROM Accounts
+    UNION ALL
+    SELECT * FROM (VALUES ('Low Salary', 0), ('Average Salary', 0), ('High Salary', 0)))
+SELECT r.category, SUM(r.is_real) as accounts_count
+FROM rates AS r
+GROUP BY r.category;
